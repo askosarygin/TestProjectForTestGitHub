fcba780509543fa9d8536e51dfade6778a04f1e7
Andrey Kosarygin <gaio-gulio-cesare@yandex.ru>
2022-09-27 15:27:02 +0300

    Сделал чтобы подгружался список сообщений

--- app/src/main/java/com/example/testprojectfortestgithub/MainActivity.kt
+++ app/src/main/java/com/example/testprojectfortestgithub/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Изменения task_1")
+        println("Сделал чтобы подгружался список сообщений")
     }
 }
